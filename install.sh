b8c69c7b170496168e0ded30c6fd85e8fe95a940
Mdb1011 <89591649+Mdb1011@users.noreply.github.com>
2021-10-10 19:31:24 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-# Project name : Termux-YTD
-# Coded by: Khansaad1275 (You dont become a coder by just changing the credits)
-# Github: https://github.com/khansaad1275/Termux-YTD
-# Date : 14/4/2020
-
-#Probably make a varible with the file name 
-#make it more easy to update!
+#mmm
 echo -e "\e[035m"  "Updating default packages\n"
 apt update && apt upgrade -y
 
@@ -41,4 +33,3 @@ mv termux-url-opener ~/bin/
 echo -e "\n"
 echo -e "\e[032m" "Process Complete!"
 echo -e "\e[032m" "Now you can share any Youtube video with Termux and you will be ask to select the quality of your downloaded video and after that,It will be automatically Downloaded"
-echo -e "\e[033m" "For More Awesome and Useful Tool like this Visit My website ©www.LearnTermux.tech"
